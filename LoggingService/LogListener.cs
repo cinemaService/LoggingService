ce35c3c32bd28a27b56ffb480eeaa50e96d4d140
Marcin Knap <marcknap@gmail.com>
2016-09-02 19:55:54 +0200

    Logging Service added

---
+++ LoggingService/LogListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractService;
+using LoggingServiceModel;
+using System.IO;
+
+namespace LoggingService
+{
+	public class LogListener : IMessageEventHandler<LogMessage>
+	{
+		StreamWriter log;
+
+		public LogListener()
+		{
+			log = new StreamWriter(Config.LogFileName);
+			log.Write(Config.InitialLogText);
+		}
+		public void close()
+		{
+			log.Close();
+		}
+		public void  onMessage(LogMessage message)
+		{
+			string logType = LogMessage.LogTypeMap[message.MessageType];
+			string logStr = String.Format("{0} :     {1} :    {2}", logType, message.ServiceName, message.Message);
+			log.WriteLine(logStr);
+			Console.WriteLine(logStr);
+		}
+	}
+}
---
+++ LoggingService/LoggingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractService;
+using LoggingServiceModel;
+
+
+namespace LoggingService 
+{
+	class LoggingService : AService<LogMessage>
+	{
+		
+        public LoggingService(string name) : base(name)
+		{
+		}
+
+		public void listen()
+		{
+			LogListener listener = new LogListener();
+			listen(listener, Config.Url, Config.QueueName);
+			listener.close();
+		}
+		static void Main(string[] args)
+		{
+			LoggingService logger = new LoggingService("Logging Service");
+			logger.listen();
+		}
+	}
+}
